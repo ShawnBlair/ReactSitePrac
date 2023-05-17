3cda72a2181b53f595f19c5c5d11e242a56fb603
ShawnBlair <shawnmwa98@gmail.com>
2023-05-17 20:07:31 +0300

    React Site Prac Project to push to GitHub

---
+++ Components/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Main(){
+    return(<div>
+        <p className="ff">Fun facts about React</p>
+        <ul type="disk" className="ff2"><li>Was first realeased in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprice apps,including mobile apps</li></ul>
+        </div>
+    )
+}
---
+++ Components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar(){
+    return(<div className="navb">
+    
+        <p className="p1">React Facts</p>
+        <p className="p2">React Course - Project 1</p>
+        </div>
+    )
+}//<img src="https://icons8.com/icon/123603/react-native" />
